e026b1914cf4e81a8cd920ebdba1c0db2d0f8d88
ShazleenAziem <shazleenaziem10@gmail.com>
2023-03-08 01:08:28 +0800

    log in

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+// Retrieve form data
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Connect to the database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "pr_parking";
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Query the database to check if the user exists
+$sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) == 1) {
+    // User exists, start a session and redirect to dashboard page
+    session_start();
+    $_SESSION['username'] = $username;
+    header("Location: dashboard.php");
+} else {
+    // User doesn't exist, show an error message
+    echo "Invalid username or password";
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
---
+++ test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Using Bootstrap in PHP</title>
+	<!-- Include the Bootstrap CSS file -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+		body {
+			background-color: #BF2C34; /* Set a default background color for the body */
+		}
+	</style>
+</head>
+
+<body>
+	<div class="bg-primary"></div>
+		<div class="container">
+			<h1>Using Bootstrap in PHP</h1>
+			<button class="btn btn-primary">Click Me</button>
+		</div>
+		<!-- Include the Bootstrap JS file -->
+		<script src="js/bootstrap.min.js"></script>
+
+		<div class="bg-light" style="background-color: #F5C26B;">
+			This element has a light gray background color.
+		</div>
+	
+
+</body>
+
+</html>
