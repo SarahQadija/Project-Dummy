20b4a3c89a44d0d0f88d698128c0aa92c880388a
Azwa09 <azwaaleeya09@gmail.com>
2023-03-16 01:09:45 +0800

    new

--- pagebypage.php
+++ pagebypage.php
@@ -9,17 +9,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="projek.css">
 </head>
 
 <body class="background">
-    <h2 align="center">PEKAN RABU PARK</h2>
+    <h1 align="center">PEKAN RABU PARK</h1>
     <ul>
         <li><a  href="senarai.php">Display Parking</a></li>
         <li><a href="edit.php">Info Parking</a></li>
         <li><a href="#contact">Report Monthly Parking</a></li>
-        <li><a class="active" href="#about">Logout</a></li>
+        <li style="float:right"><a class="active" href="#about">Logout</a></li>
     </ul>
+    <p></p>
+    <table>
+        <tr>
+            <td><img src="gambar/pekanrabu.jpg" width="500" height="320">
+            <img src="gambar/car.jpg" width="500" height="320">
+            <img src="gambar/park.jpg" width="500" height="320">
+            <img src="gambar/parking.jpg" width="500" height="320"></td>
+        </tr>
+    </table>
+
 
 </body>
 
