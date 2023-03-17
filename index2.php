70d17e10cb4ec641622ba7cb00859d736f88de59
Azwa09 <azwaaleeya09@gmail.com>
2023-03-18 00:30:43 +0800

    new

--- index2.php
+++ index2.php
@@ -9,7 +9,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PEKAN RABU PARK</title>
+    <link rel="icon" href="gambar/pekanrabu.jpg" />
     <link rel="stylesheet" href="projek.css">
 </head>
 
